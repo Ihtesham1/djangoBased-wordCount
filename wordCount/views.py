142dd9193fa3019ae2ccea05616213e5934fc199
Ihtesham <qazi.m.ihtesham@gmail.com>
2019-09-25 16:50:36 +0500

    added counter to count words

--- wordCount/views.py
+++ wordCount/views.py
@@ -1,3 +1,5 @@
+import operator
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -7,4 +9,19 @@ def home(request):
 
 
 def count(request):
-    return render(request, 'count.html')
+    fulltext = request.GET['fulltext']
+    wordcount = fulltext.split()
+
+    worddict = {}
+    for word in wordcount:
+        #it will add in the existing word
+        if word in worddict:
+            worddict[word] += 1
+        else:
+            #it will start counting when new word is found
+            worddict[word] = 1
+
+
+    sorteddict = sorted(worddict.items(), key=operator.itemgetter(1), reverse=True)
+
+    return render(request, 'count.html', {'fulltext': fulltext, 'wordcount': len(wordcount), 'SortedDict': sorteddict})
